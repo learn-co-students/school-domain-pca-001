99ad6e7c027b519abb504efd0830573e89745c2f
Madison McCardle <madison.mccardle@powerhrg.com>
2020-05-04 17:05:42 -0400

    hashes and classes

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
-# code here!
+class School 
+    attr_reader :school, :roster
+
+    def initialize(school)
+        @school = school
+        @roster = {}
+    end
+
+    def add_student(student, grade)
+        @roster[grade] ||= []
+        @roster[grade]<<"#{student}"
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end 
+
+    def sort
+       sorted_roster= {}
+       roster.each do |grades, students|
+        sorted_roster[grades]= students.sort
+       end 
+       sorted_roster
+    end
+end
