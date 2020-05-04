cb903efb959fd4c7e0fa03d2268fe030576575e1
Pat McClernan <patmccler@gmail.com>
2020-05-04 14:24:13 -0400

    build school methods

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,21 @@
-# code here!
+class School
+  attr_reader :name, :roster
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    @roster[grade] ? @roster[grade] << name : @roster[grade] = [name]
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each_with_object({}) do |(grade,students), result|
+      result[grade] = students.sort
+    end
+  end
+end
