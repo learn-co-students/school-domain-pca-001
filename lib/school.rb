7716168af5b0fe3da9a95e913e2cc628eeea29b8
Andrew Kloecker <andrew.kloecker@MacBook-Pro.local>
2020-05-04 14:37:55 -0400

    lab complete

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,20 @@
 # code here!
+class School
+    attr_accessor :name, :roster
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+    def add_student(name, grade)
+        @roster.key?(grade) ? @roster[grade].push(name) : @roster[grade] = [name]
+    end
+    def grade(number)
+        @roster[number]
+    end
+    def sort()
+        keys = @roster.keys.sort()
+        hash = {}
+        keys.each{ |key| hash[key] = @roster[key].sort() }
+        hash
+    end
+end
